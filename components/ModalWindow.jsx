d9085353679ee536af6fb728fb12872596192b8a
dima_samborskyi <samborskiydmitriy1@gmail.com>
2024-01-16 21:07:45 +0200

    fix

--- components/ModalWindow.jsx
+++ components/ModalWindow.jsx
@@ -7,8 +7,8 @@ import * as yup from "yup";
 
 const validationSchema = yup.object({
   title: yup.string().required("Title is required"),
-  timeFrom: yup.date(),
-  timeTo: yup.date(),
+  timeFrom: yup.string(),
+  timeTo: yup.string(),
 });
 
 const hoursToMinutes = (timeFrom, timeTo) => {
